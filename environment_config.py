c91af3351ff43f26765b09773044d817b8380a00
Milagros <mirakel1401@gmail.com>
2020-03-27 10:25:11 -0500

    configuracion necesario para la  base de datos [EC-11]

--- environment_config.py
+++ environment_config.py
@@ -10,3 +10,5 @@ class EnvironmentConfig:
     PORT = os.environ.get('PORT', 8080)
     TEMPLATE_DIR = os.environ.get('TEMPLATE_DIR', 'web/templates')
     SECRET_KEY = os.environ.get('SECRET_KEY', 'M#lOkNdmResearchPerudAxaGS=GgEPl)&9_$JFNCE&djMPB30zwRwvMDQxFq&tT=)')
+
+    DATABASE = os.environ.get('DATABASE', 'sqlite:///epidemiologic.db')
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import jinja2
-from environment_config import EnvironmentConfig
 from flask import Flask
+from flask_injector import FlaskInjector
+from flask_sqlalchemy import SQLAlchemy
+from injector import Binder, singleton
+from sqlalchemy import MetaData
+
+from environment_config import EnvironmentConfig
+from repositories.configuration import configure_repositories_binding
+from repositories.sqlalchemy.mapping.user_mapping import user_mapping
 from web.configuration import configure_web_route
 from api.routes import api_bp
 
@@ -13,12 +20,44 @@ ROUTING_MODULES = [
 ]
 
 
-def create_app(templates_folders_list=templates_folders):
+def configure_database_bindings(binder: Binder) -> Binder:
+    application = binder.injector.get(Flask)
+    metadata = MetaData()
+    try:
+        user_mapping(metadata)
+        pass
+    except Exception as e:
+        print(e)
+
+    db = SQLAlchemy(application)
+
+    metadata.reflect(db.engine)
+    metadata.drop_all(db.engine)
+    db.session.commit()
+
+    metadata.create_all(db.engine)
+    db.session.commit()
+
+    binder.bind(SQLAlchemy, to=db, scope=singleton)
+    return binder
+
+
+modules_list = [
+    configure_repositories_binding,
+    configure_database_bindings
+]
+
+
+def create_app(templates_folders_list=templates_folders, modules=modules_list):
     application = Flask(__name__)
 
     application.register_blueprint(api_bp)
     application.config['SECRET_KEY'] = EnvironmentConfig.SECRET_KEY
     application.config['TESTING'] = True
+    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    application.config.update(
+        SQLALCHEMY_DATABASE_URI=EnvironmentConfig.DATABASE
+    )
 
     for routing in ROUTING_MODULES:
         routing(application)
@@ -28,6 +67,7 @@ def create_app(templates_folders_list=templates_folders):
         jinja2.FileSystemLoader(templates_folders_list)
     ])
 
+    FlaskInjector(app=application, modules=modules)
     application.jinja_loader = custom_loader
 
     return application

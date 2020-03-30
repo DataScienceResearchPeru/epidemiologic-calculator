ad2aac6073f55095e5f6549aee385513ccf356a3
Randy Real <randyterros@gmail.com>
2020-03-30 03:27:59 -0500

    add cors library and implement to recive external request.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask import Flask
 from web.configuration import configure_web_route
 from api.routes import api_bp
 
+from flask_cors import CORS, cross_origin
+
 templates_folders = [
     EnvironmentConfig.TEMPLATE_DIR,
 ]
@@ -19,6 +21,9 @@ def create_app(templates_folders_list=templates_folders):
     application.register_blueprint(api_bp)
     application.config['SECRET_KEY'] = EnvironmentConfig.SECRET_KEY
     application.config['TESTING'] = True
+    
+    cors = CORS(application)
+    application.config['CORS_HEADERS'] = 'Content-Type'
 
     for routing in ROUTING_MODULES:
         routing(application)
